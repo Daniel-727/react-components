b83cfe7cfadbbe5fe5647ac8da80a8814bda7235
Daniel Li <zaoster777@gmail.com>
2022-02-23 13:52:15 -0600

    Udemy react-components practice

---
+++ components/Heading.jsx
@@ -0,0 +1,8 @@
+import React from 'react'; //importing react so the code below can be compiled properly using babel
+
+function Heading(props){
+    return <h1>My Favourite Foods</h1>
+}
+
+
+export default Heading; //exporting Heading so it can be used by index.js
---
+++ components/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; //importing react so the code below can be compiled properly using babel
+
+function List(props){
+    return (
+        <ul>
+            <li>Bacon</li>
+            <li>Jamon</li>
+            <li>Noodles</li>
+        </ul>
+    )
+}
+
+
+export default List; //exporting List so it can be used by index.js
